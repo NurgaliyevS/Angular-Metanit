96a5b2b3c4f8dc5c2f0facb4f46d9d0c82f83531
NurgaliyevS <nurgaliyev000@gmail.com>
2023-03-07 12:37:48 +0600

    simple form with validation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,22 @@
 import { Component } from '@angular/core';
+export class User {
+  constructor(
+    public name: string,
+    public email: string,
+    public phone: string
+  ) {}
+}
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
+  styles: [`
+  input.ng-touched.ng-invalid {border:solid red 2px;}
+  input.ng-touched.ng-valid {border:solid green 2px;}
+`]
 })
-export class AppComponent {}
+export class AppComponent {
+  user: User = new User('', '', '');
+  addUser(){
+    console.log(this.user);
+  }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { NgModule } from '@angular/core';
-import { DataComponent } from './data.component';
-import { DataService } from './data.service';
-import { LogService } from './log.service';
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent, DataComponent],
-  bootstrap: [AppComponent],
-  providers: [DataService, LogService]
+  declarations: [AppComponent],
+  bootstrap: [AppComponent]
 })
 export class AppModule {}
